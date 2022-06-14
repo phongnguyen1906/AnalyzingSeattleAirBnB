4f60fbf1c12b0aa4d93a3bc3e2bbc286e520835b
phongnguyen1906 <44720651+phongnguyen1906@users.noreply.github.com>
2022-06-14 22:41:48 +0700

    Update process_data.py

--- 2. Disaster Response Pipeline/data/process_data.py
+++ 2. Disaster Response Pipeline/data/process_data.py
@@ -55,7 +55,7 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     engine = create_engine('sqlite:///{}.db'.format(database_filename))  
-    df.to_sql('message_table', engine, index=False)
+    df.to_sql('message_table', engine, index=False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
